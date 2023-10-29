6f100d2c07f63bf663a15d9875f62a6665f955f8
lynamjd <lynamjd@outlook.com>
2023-10-29 22:49:10 +0000

    initial commit for comments exercise

---
+++ src/freecodecamp/CreateAndUseCodeComments/Comments.cs
@@ -0,0 +1,13 @@
+public class Comments
+{
+    public static void Run()
+    {
+        string firstName = "Bob";
+        int widgetsPurchased = 8;
+        // Testing a chance to the message.
+        // int widgetsSold = 7;
+
+        // Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+        Console.WriteLine($"{firstName} sold {widgetsPurchased} widgets.");
+    }
+}
